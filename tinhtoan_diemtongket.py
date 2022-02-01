a563b667c201d011b5bf09cb3a9fd35dc958ea40
datnguyen_technology <85825253+NguyenQuocDat-Mechatronic@users.noreply.github.com>
2022-02-01 12:59:52 +0700

    compelted bài 1

--- tinhtoan_diemtongket.py
+++ tinhtoan_diemtongket.py
@@ -10,13 +10,11 @@ def tinhdiem_trungbinh(x):
                 dict_subject.append(i.strip())
         break
     total_dict = dict()
-    list_MaHS = list()
     for sub_line in handle:
         if not sub_line.startswith("Ma HS"):
             list_point = list()
             for point in sub_line.split(";"):
                 list_point.append(point.strip())
-            avg = 0
             sub_dict =dict()
             i = 0
             for point_cal in list_point:
@@ -30,11 +28,17 @@ def tinhdiem_trungbinh(x):
                     sub_dict[dict_subject[1+i]] = avg
                     i +=1
         total_dict[list_point[0]] = sub_dict
-    print(total_dict)
-    return total_dict
+    return string_subject,total_dict
 def luudiem_trungbinh(y,z):
-    with open(y,mode='w') as f:
-        f.write(z)
+    f = open(y,mode="w")
+    f.write(z[0])
+    total_dict = z[1]
+    for mahs, point in total_dict.items():
+        string_point = str()
+        for sub_point in point.values():
+            string_point =  str(sub_point)+","+string_point
+        str_final = str(mahs) +";"+ string_point[:-1]+"\n"
+        f.write(str_final)
 def main():
     link_in_file = input()
     tinhdiem_trungbinh(link_in_file)
@@ -42,6 +46,8 @@ def main():
     luudiem_trungbinh(link_out_file,tinhdiem_trungbinh(link_in_file))
 
 main()
+"""Test thử điểm trung bình"""
 # tinhdiem_trungbinh(".\.\diem_chitiet.txt")
 # tinhdiem_trungbinh("D:\1.Education\1.IT\1_git\1_assignment1\2_assignment_2_funix\diem_chitiet.txt")
 # luudiem_trungbinh("D:\1.Education\1.IT\1_git\1_assignment1\2_assignment_2_funix\diem_trungbinh.txt")
+
